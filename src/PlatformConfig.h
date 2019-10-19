b51d9bff588aee264f2797052e161b9c020a48e4
Drew Tayman <dretay@gmail.com>
2019-10-19 14:03:55 -0400

    fixing function prototype

--- src/PlatformConfig.h
+++ src/PlatformConfig.h
@@ -8,7 +8,7 @@ typedef struct PlatformConfig {
     void (*spi_transmit)(u8* out, size_t out_size);
     void (*spi_transmit_receive)(u8* out, size_t out_size, u8* in, size_t in_size);
     void (*hard_reset)(void);
-    void (*delay)(int ms);
+    void (*delay)(u32 ms);
     u32 (*millis)(void);
 
     void (*log_log)(bool append, bool terminate, int level, const char* file, const char* function, int line, const char* fmt, ...);
